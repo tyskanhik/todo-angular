57dd07eb65c9f9f52c1b958e266ddb424529a6df
Arctic Fox <tyskanhik@mail.ru>
2025-06-23 11:53:41 +0300

    создан список с задачами

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TaskListComponent } from "./features/task-list/task-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true
